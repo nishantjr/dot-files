61256120244a060feb3d24044b5a7cae7ad44091
Nishant Rodrigues <nishant.rodrigues@infinetfs.com>
2014-08-31 22:54:56 +0900

    Add function key shortcuts for network, backlight, volume. Also for disabling shortcuts.

---
+++ lib/utils.bash
@@ -0,0 +1,31 @@
+
+function graphical_fail() {
+    notify-send -u critical "$1"
+}
+
+function check_binary() {
+    local bin=$1
+    type $bin >/dev/null 2>&1 && return
+    graphical-fail "'$bin' is not installed."
+    exit 1
+}
+
+function bash_enable_exit_on_error() {
+    set -e
+}
+
+function print_percent_intervals() {
+    for level in $(seq 0 10 100); do
+        echo $level
+    done
+}
+
+function dmenu_command() {
+    local title=$1
+    local input_gen=$2
+    local answer_handler=$3
+
+    answer=$($input_gen | dmenu -p "$title:") || return
+    echo >&2 "$title: $answer"
+    $answer_handler "$answer"
+}
